a2533334946c2f4de85972587f00e87bac85f133
impalakdwivedi <aapalakdwivedi@gmail.com>
2021-07-14 03:08:34 +0530

    this is my first commit i am in doubt

--- Extension/main.js
+++ Extension/main.js
@@ -4,14 +4,15 @@ var onofftext = document.getElementById("onoff");
 
 //Function that executes when toggle is switched.
 maintoggle.onclick = function() {
-    var onofftext = document.getElementById("onoff");
-    if (maintoggle.checked == true) {
-        localStorage.setItem('toggle', JSON.stringify(1));
-        onofftext.innerText = "Mining on! No ads!";
-        chrome.tabs.reload();
-    } else {
-        localStorage.setItem('toggle', JSON.stringify(0));
-        onofftext.innerText = "Ads are being displayed.";
-        chrome.tabs.reload();
+        var onofftext = document.getElementById("onoff");
+        if (maintoggle.checked == true) {
+            localStorage.setItem('toggle', JSON.stringify(1));
+            onofftext.innerText = "Mining on! No ads!";
+            chrome.tabs.reload();
+        } else {
+            localStorage.setItem('toggle', JSON.stringify(0));
+            onofftext.innerText = "Ads are being displayed.";
+            chrome.tabs.reload();
+        }
     }
-}
+    //let's see if on this laptopi am able to make my commits
